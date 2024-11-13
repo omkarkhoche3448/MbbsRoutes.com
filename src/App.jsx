46599dfb8e720b4f627fc3d0dacb643c9832a555
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2024-11-14 01:37:31 +0530

    file deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
 import ChatBot from "./components/chatBotui/ChatBot";
-import Consultancypages from "./pages/Consultancypages";
+// import Consultancypages from "./pages/Consultancypages";
 import Landingpage from "./pages/Landingpage";
 import AboutPage from "./pages/AboutPage";
 import MBBSEducationAbroad from "./pages/MBBSEducationAbroad";
@@ -16,10 +16,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Landingpage />} />
-          <Route
+          {/* <Route
             path="/consultancy/:countryName/mbbs"
             element={<Consultancypages />}
-          />
+          /> */}
           <Route path="/about-us" element={<AboutPage />} />
           <Route path="/mbbs-in-abroad" element={<MBBSEducationAbroad />} />
           <Route
