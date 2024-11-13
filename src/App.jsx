dfbf858c1c96eb1e55d1d3303538f4b50ad50504
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2024-11-14 01:24:28 +0530

    Chatbot UI added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/common/Navbar";
-import Consultancypage from './pages/Consultancypage';
+import ChatBot from "./components/chatBotui/ChatBot";
+import Consultancypage from "./pages/Consultancypage";
 import Landingpage from "./pages/Landingpage";
 import AboutPage from "./pages/AboutPage";
 import MBBSEducationAbroad from "./pages/MBBSEducationAbroad";
 import CollegeRecommendationpage from "./pages/CollegeRecommendationpage";
-import Footer from "./components/common/Footer"
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
@@ -21,9 +22,13 @@ function App() {
           />
           <Route path="/about-us" element={<AboutPage />} />
           <Route path="/mbbs-in-abroad" element={<MBBSEducationAbroad />} />
-          <Route path ="/mbbs-in-abroad/college-recomendation" element={<CollegeRecommendationpage />} />
+          <Route
+            path="/mbbs-in-abroad/college-recomendation"
+            element={<CollegeRecommendationpage />}
+          />
         </Routes>
-        <Footer/>
+        <ChatBot />
+        <Footer />
       </Router>
     </div>
   );
