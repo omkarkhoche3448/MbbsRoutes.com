8f278ccca3b9c9454774f875eddb8d3fe5ba5425
Omkar Khoche <omkarkhoche3448@gmail.com>
2025-03-12 09:21:14 +0530

    --amend

--- Frontend/src/Pages/Landingpage.jsx
+++ Frontend/src/Pages/Landingpage.jsx
@@ -5,14 +5,18 @@ import Info from "../components/landingpage/Info";
 import Footer from "../components/common/Footer";
 import FlagBanner from "../components/landingpage/FlagBanner";
 import FAQSection from "../components/common/FAQSection";
+// import StudyAbroadSection from "../components/landingpage/StudyAbroadSection"
+import VideoSection from "../components/landingpage/VideoSection";
 
 function Landingpage() {
   return (
     <div>
       <HeroSection />
       <FlagBanner />
+      {/* <StudyAbroadSection/> */}
       <InfoSection />
       <Info />
+      <VideoSection/>
       <FAQSection />
     </div>
   );
